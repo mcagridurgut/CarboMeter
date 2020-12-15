020c877c47bed89c39af3f037aae207382a5cf96
mcagri <mcagridurgut@gmail.com>
2020-12-15 23:17:52 +0300

    update method added

--- src/DatabaseConnection.java
+++ src/DatabaseConnection.java
@@ -1,12 +1,13 @@
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.NoSuchElementException;
 
 public class DatabaseConnection {
     static int ROWSIZE = 10;
+
+    static String wd = System.getProperty("user.dir").replace('\\','/');
+    static String url = "jdbc:sqlite:"+ wd +"/carbopoints.db";
     public static void createNewTableIfNotExists() {
-        // SQLite connection string
-        String wd = System.getProperty("user.dir").replace('\\','/');
-        String url = "jdbc:sqlite:"+ wd +"/carbopoints.db";
 
         // SQL statement for creating a new table
         String sql = "CREATE TABLE IF NOT EXISTS users (\n"
@@ -33,8 +34,6 @@ public class DatabaseConnection {
 
     private static Connection connect() {
         // SQLite connection string
-        String wd = System.getProperty("user.dir").replace('\\','/');
-        String url = "jdbc:sqlite:"+ wd +"/carbopoints.db";
         Connection conn = null;
         try {
             conn = DriverManager.getConnection(url);
@@ -43,24 +42,54 @@ public class DatabaseConnection {
         }
         return conn;
     }
-    public static void updateUser(String name, double carbopoint, double transport,double housing,double electronic,double other ){
-       Object[] user =  select(name);
-       createNewUser((String)user[1]+"-old-",(String)user[2],(double)user[3],(double)user[4],(double)user[5],(double)user[6],(double)user[7],(Integer)user[8],(Integer)user[9]);
-       createNewUser((String)user[1],(String) user[2], carbopoint,transport,housing,electronic,other,(Integer)user[8],(Integer)user[9]);
+    public static boolean isSuchUserExists(String name){
+        Object[] arr  = select(name);
+        return (arr != null && arr[0] != null);
     }
-    public static void createNewUser(String name, String password, double carbopoint, double transport,double housing,double electronic,double other, int userType, int familyNumber ) {
-        createNewTableIfNotExists();
-        Object[] array = new Object[ROWSIZE];
+    public static void updateUser(String name, String password, double carbopoint, double transport,double housing,double electronics,double other, int userType, int familyNumber ) throws NoSuchElementException{
+        if(isSuchUserExists(name)) {
+            Object[] user = select(name);
+            if (!isSuchUserExists((String) user[1] + "-old-"))
+                createNewUser((String) user[1] + "-old-", (String) user[2], (double) user[3], (double) user[4], (double) user[5], (double) user[6], (double) user[7], (Integer) user[8], (Integer) user[9]);
+            else
+                update((String) user[1] + "-old-", (String) user[2], (double) user[3], (double) user[4], (double) user[5], (double) user[6], (double) user[7], (Integer) user[8], (Integer) user[9]);
+            update(name, password, carbopoint, transport, housing, electronics, other, userType, familyNumber);
+            return;
+        }
+        throw new NoSuchElementException("no such user");
+    }
 
+    public static Object[] getOldValues(String name){
+        return select(name+"-old-");
+    }
+
+    private static void update(String name, String password, double carbopoint, double transport, double housing, double electronics, double other, int userType, int familyNumber) {
+        Object[] array = new Object[ROWSIZE];
+        String sql = "UPDATE users SET password = '"+password+"' WHERE name  = '" + name + "';";
+//                "carbopoint = "+ carbopoint +1
+//                ",transport = "+ transport +",housing = "+ housing +",electronics = "+electronics+", other = "+ other +",userType = "+ userType +
+//                ",familyNumber = "+familyNumber+" WHERE name = "+name;
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement stmt = conn.createStatement()) {
+            stmt.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void createNewUser(String name, String password, double carbopoint, double transport,double housing,double electronics,double other, int userType, int familyNumber ){
+        createNewTableIfNotExists();
+        if(!isSuchUserExists(name)) {
+            Object[] array = new Object[ROWSIZE];
             String sql = "INSERT INTO users(name, password, carbopoint,transport,housing,electronics,other,userType,familyNumber ) VALUES(?,?,?,?,?,?,?,?,?)";
             try (Connection conn = connect();
-                 PreparedStatement pstmt  = conn.prepareStatement(sql)) {
+                 PreparedStatement pstmt = conn.prepareStatement(sql)) {
                 pstmt.setString(1, name);
                 pstmt.setString(2, password);
                 pstmt.setDouble(3, carbopoint);
                 pstmt.setDouble(4, transport);
                 pstmt.setDouble(5, housing);
-                pstmt.setDouble(6, electronic);
+                pstmt.setDouble(6, electronics);
                 pstmt.setDouble(7, other);
                 pstmt.setInt(8, userType);
                 pstmt.setInt(9, familyNumber);
@@ -68,6 +97,7 @@ public class DatabaseConnection {
             } catch (SQLException e) {
                 System.out.println(e.getMessage());
             }
+        }
     }
     public static ArrayList<Object[]> selectAll(){
         createNewTableIfNotExists();
@@ -98,7 +128,7 @@ public class DatabaseConnection {
         }
         return arrayList;
     }
-    public static Object[] select(String name){
+    public static Object[] select(String name)  {
         createNewTableIfNotExists();
         Object[] array = new Object[ROWSIZE];
         String sql = "SELECT * FROM users WHERE name = '"+name+"'";
@@ -121,7 +151,7 @@ public class DatabaseConnection {
                 array[9]= rs.getInt("familyNumber");
             }
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            return null;
         }
         return array;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,16 @@
+import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        //DatabaseConnection.createNewUser("second", "hello",1,2,3,4,5,6,7);
-//        ArrayList<Object[]> arrayList = DatabaseConnection.selectAll();
-//        for(int j = 0; j<arrayList.size();j++ ) {
-//            Object[] arr = arrayList.get(j);
-//            for (int i = 0; i < DatabaseConnection.ROWSIZE; i++) {
-//                System.out.print(arr[i] + "\t");
-//            }System.out.println("");
-//        }
-            Object[] arr = DatabaseConnection.select("third");
-            if(arr == null)
-                System.out.println("it is null");
-            else {
-                for (int i = 0; i < DatabaseConnection.ROWSIZE; i++) {
-                    System.out.print(arr[i] + "\t");
-                }
-            }
+        DatabaseConnection.updateUser("fifth", "kelvlo",1,2,3,4,5,6,7);
+        ArrayList<Object[]> arrayList = DatabaseConnection.selectAll();
+        for(int j = 0; j<arrayList.size();j++ ) {
+            Object[] arr = arrayList.get(j);
+            for (int i = 0; i < DatabaseConnection.ROWSIZE; i++) {
+                System.out.print(arr[i] + "\t");
+            }System.out.println("");
+        }
+
     }
 }
