102e0171fe7c182d93eefd7d1ea347e7f8128c3d
Alper Mumcular <alper.mumcular@ug.bilkent.edu.tr>
2020-12-27 22:40:05 +0300

    javadoc added to JavaMail and JavaMailUtil classes

--- src/mail/JavaMailUtil.java
+++ src/mail/JavaMailUtil.java
@@ -13,7 +13,18 @@ import javax.mail.internet.AddressException;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 
+/**
+ * JavaMailUtil class for sending email
+ * @author Alper Mumcular
+ * @version 27.12.2020
+ */
 public class JavaMailUtil {
+	/**
+	 * Sends the email
+	 * @param recipient --> email of the recipient
+	 * @param subject --> subject of the email
+	 * @param mail --> details of the email
+	 */
 	public static void sendMail(String recipient, String subject, String mail) throws Exception {
 		System.out.println("Preparing to send email");
 		Properties properties = new Properties();
@@ -38,7 +49,16 @@ public class JavaMailUtil {
 		Transport.send(message);
 		System.out.println("Message sent successfully.");
 	}
-	
+
+	/**
+	 * Prepares the message
+	 * @param session --> Session
+	 * @param subject --> subject of the mail
+	 * @param mail --> details of the mail
+	 * @param myAccountEmail --> carbopoint app email
+	 * @param recipient --> email of the user
+	 * @return returns the message
+	 */
 	private static Message prepareMessage(Session session, String subject, String mail, String myAccountEmail, String recipient) {
 		try {
 			Message message = new MimeMessage(session);
